2d941c7070793e74855735157233e87701731594
Austin Wiginton <ahwig93@gmail.com>
2023-03-25 02:37:02 -0500

    Change params for navbar buttons and fetch movie data from API

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,24 @@
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
+const API_KEY = process.env.API_KEY;
 
-const inter = Inter({ subsets: ['latin'] });
+async function Home({ searchParams }) {
+  const genre = searchParams.genre;
+  let request;
 
-export default function Home() {
+  if (genre == undefined) {
+    request = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`;
+  } else {
+    request = `https://api.themoviedb.org/3/movie/${genre}?api_key=${API_KEY}`;
+  }
+
+  const res = await fetch(request);
+  const data = await res.json();
+  const results = data.results;
+  console.log(results);
   return (
     <div>
       <h1>Home Page</h1>
     </div>
   );
 }
+
+export default Home;
