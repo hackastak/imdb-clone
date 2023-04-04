4ec2fae0225150e6358a3ff590b19d76966174dc
Austin Wiginton <ahwig93@gmail.com>
2023-04-04 02:03:55 -0500

    Add overview to back of cards and add the functionality to click on the cards to flip them

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,27 +1,46 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 const API_KEY = process.env.API_KEY;
 
 function MovieCard(props) {
+  const [isFlipped, setIsFlipped] = useState(false);
   let posterPath = props.result.poster_path;
-
   let imgRequestURL = `https://image.tmdb.org/t/p/w500/${posterPath}`;
 
   return (
     <div
-      className='w-1/6 m-5 bg-gray-800 flex flex-col border-0 rounded-lg'
+      className={`movie-card w-1/4 m-5 bg-gray-800 flex flex-col border-0 rounded-lg
+      ${isFlipped ? 'flipped' : ''}
+      `}
+      onClick={() => {
+        setIsFlipped(!isFlipped);
+      }}
       key={props.result.id}
     >
-      <div>
-        <img
-          className='border-0 rounded-t-lg'
-          src={imgRequestURL}
-          alt='Movie Poster'
-        />
-      </div>
-      <div className=''>
-        <h2>{props.result.title}</h2>
-      </div>
+      {!isFlipped ? (
+        <div className='card-front'>
+          <div>
+            <img
+              className='border-0 rounded-t-lg'
+              src={imgRequestURL}
+              alt='Movie Poster'
+            />
+          </div>
+          <div className='w-full text-center p-2'>
+            <h2>{props.result.title}</h2>
+          </div>
+        </div>
+      ) : (
+        <div className='card-back'>
+          <div className='w-full text-center p-2'>
+            <h2 className='text-white'>{props.result.title}</h2>
+          </div>
+          <div>
+            <p className='px-3'>{props.result.overview}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
